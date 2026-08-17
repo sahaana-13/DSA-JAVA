abf08f8f06a5d7f2d71f3db1f8eb454d3d554352
sahaana-13 <sahaanapushparaj@gmail.com>
2026-08-17 22:25:38 +0530

    largest element in an array

---
+++ arrays/largest_element.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.*;
+
+
+
+public class largest_element{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter the element");
+            arr[i] = sc.nextInt();
+
+        }
+
+        int max = arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.print("largest element:" + max);
+
+    }
+}
