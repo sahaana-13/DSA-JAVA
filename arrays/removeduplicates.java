27c68fd3f1f82c5e8f0e489e09c632c0b5ca7f7c
sahaana-13 <sahaanapushparaj@gmail.com>
2026-08-20 20:52:04 +0530

    did remove duplicates

---
+++ arrays/removeduplicates.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.*;
+
+public class removeduplicates {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        int count = 1;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int current = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            if (current != arr[i]) {
+                count++;
+                arr[count - 1] = arr[i];
+                current = arr[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
